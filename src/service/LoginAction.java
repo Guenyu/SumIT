a87212a52304f2ae7bee9ba053a26ec9b36692d0
ljk5446 <areyoua@naver.com>
2016-04-04 16:38:11 +0900

    수정완료

--- src/service/LoginAction.java
+++ src/service/LoginAction.java
@@ -2,6 +2,8 @@ package service;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import project.*;
 public class LoginAction implements CommandProcess {
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
@@ -11,7 +13,10 @@ public class LoginAction implements CommandProcess {
 		
 		UserInFoDao uf=UserInFoDao.getInstance();
 		int result=uf.loginchk(email, password);
-		
+		if (result > 0) {
+			HttpSession session = request.getSession();
+			session.setAttribute("email", email);
+		}
 		request.setAttribute("result", result);
 		request.setAttribute("email", name);
 	
